005a5d75dc690b2a18007c469e4e3986b3d0f0de
Martina Vincheva <tufituri@gmail.com>
2021-03-24 14:51:44 +0200

    generateTerranFleet

--- Starcraft/src/BattleField.c
+++ Starcraft/src/BattleField.c
@@ -1,33 +1,64 @@
 #include "BattleField.h"
+#include "terranShip.h"
 
-void generateTerranFleet(BattleField *battleField, const char *terranFleetStr) {
-}
+void generateTerranFleet(BattleField *battleField, const char *terranFleetStr)
+{
+    int fleetSize = strlen(terranFleetStr);
+    Vector *terranFleet = &battleField->terranFleet;
 
-void generateProtossFleet(BattleField *battleField, const char *protossFleetStr) {
-}
-
-void startBattle(BattleField *battleField) {
-  while (true) {
-    if (processTerranTurn(battleField)) {
-      printf("TERRAN has won!\n");
-      break;
+    vectorInit(terranFleet, sizeof(terranFleetStr));
+    for (int idx = 0; idx < fleetSize; idx++)
+    {
+        switch (terranFleetStr[idx])
+        {
+        case 'v':
+        case 'V':
+        {
+            vectorPush(terranFleet, createVikingShip(idx));
+            break;
+        }
+        case 'b':
+        case 'B':
+        {
+            vectorPush(terranFleet, createBattleShip(idx));
+            break;
+        }
+        }
     }
+}
 
-    if (processProtossTurn(battleField)) {
-      printf("PROTOSS has won!\n");
-      break;
+void generateProtossFleet(BattleField *battleField, const char *protossFleetStr)
+{
+}
+
+void startBattle(BattleField *battleField)
+{
+    while (true)
+    {
+        if (processTerranTurn(battleField))
+        {
+            printf("TERRAN has won!\n");
+            break;
+        }
+
+        if (processProtossTurn(battleField))
+        {
+            printf("PROTOSS has won!\n");
+            break;
+        }
     }
-  }
 }
 
-void deinit(BattleField *battleField) {
+void deinit(BattleField *battleField)
+{
 }
 
-bool processTerranTurn(BattleField *battleField) {
-  return false;
+bool processTerranTurn(BattleField *battleField)
+{
+    return false;
 }
 
-bool processProtossTurn(BattleField *battleField) {
-  return false;
+bool processProtossTurn(BattleField *battleField)
+{
+    return false;
 }
-
